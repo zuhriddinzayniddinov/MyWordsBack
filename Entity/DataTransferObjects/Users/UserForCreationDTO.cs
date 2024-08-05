b2f12a890a7a530a573070af79625311bf95e9d2
z_zayniddinov <zayniddinovzuhriddin99@gmail.com>
2024-08-05 09:17:42 +0500

    role

--- Entity/DataTransferObjects/Users/UserForCreationDTO.cs
+++
@@ -1,9 +0,0 @@
-namespace Entity.DataTransferObjects;
-
-public record UserForCreationDTO(
-    string userName,
-    string password,
-    string firstName,
-    string lastName,
-    string midleName,
-    long stuructureId);
